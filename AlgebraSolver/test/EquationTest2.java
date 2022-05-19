ed077d99a828dfd1a5fe75ab495e48272b945598
Lucas Rodriguez <lucasrodriguez0831@gmail.com>
2022-05-18 21:36:17 -0400

    Create Second Test File

---
+++ AlgebraSolver/test/EquationTest2.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import algebrafx.Equation;
+
+public class EquationTest2 {
+    @Test
+    public void test26(){
+        var eq = new Equation("2x + 1 = 3");
+        assertTrue(eq.validateBool());
+        assertEquals("x = 1", eq.solve());
+    }
+    @Test
+    public void test27(){
+        var eq = new Equation("4.4 = 2x - 2.2");
+        assertTrue(eq.validateBool());
+        assertEquals("x = 3.3", eq.solve());
+    }
+    @Test
+    public void test28(){
+        var eq = new Equation("5.4 = -0.6 + 0.4num");
+        assertTrue(eq.validateBool());
+        assertEquals("num = 15", eq.solve());
+    }}
